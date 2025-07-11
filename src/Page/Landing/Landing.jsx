616868895cc3a78ceb960a61285abaaf42fae2fd
favour <omojolaobaloluwa@gmail.com>
2025-07-11 11:35:43 +0100

    done

--- src/Page/Landing/Landing.jsx
+++ src/Page/Landing/Landing.jsx
@@ -3,10 +3,14 @@ import SideBar from "../../Component/SideBar/SideBar";
 import "./Landing.css";
 import { FiPieChart, FiShoppingCart, FiUsers } from "react-icons/fi";
 import { TfiPieChart } from "react-icons/tfi";
-import { LuUsers } from "react-icons/lu";
+import { LuEye, LuUsers } from "react-icons/lu";
 import { TbCurrencyDollar } from "react-icons/tb";
 import { SlOptionsVertical } from "react-icons/sl";
-import { PiCurrencyDollarSimple } from "react-icons/pi";
+import {
+  PiCurrencyDollarSimple,
+  PiDotsThreeVerticalBold,
+} from "react-icons/pi";
+import { IoIosAdd } from "react-icons/io";
 import {
   Chart as ChartJS,
   LineElement,
@@ -30,6 +34,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import img from "../../assets/images/alluring-distinguished-casual-man-in-a-t-shirt-and-jeans-with-transparent-background-free-png.webp";
 import { CiMenuKebab, CiWallet } from "react-icons/ci";
+import { HiOutlineTrash } from "react-icons/hi2";
 
 // Register necessary chart components
 ChartJS.register(
@@ -49,11 +54,13 @@ const Landing = () => {
   const [prod, setProd] = useState([]);
   const [user, setUser] = useState([]);
   const [loader, setLoader] = useState(false);
+  const [Prodloader, setProdLoader] = useState(false);
+  const [load, setLoad] = useState(false);
   const navigate = useNavigate();
 
   const fetchProduct = async () => {
+    setProdLoader(true);
     try {
-      setLoader(true);
       const fetchedProduct = await axios.get(
         "https://villyzstore.onrender.com/getallProducts"
       );
@@ -82,7 +89,7 @@ const Landing = () => {
         title: error.message,
       });
     } finally {
-      setLoader(false);
+      setProdLoader(false);
     }
   };
   useEffect(() => {
@@ -247,7 +254,118 @@ const Landing = () => {
   useEffect(() => {
     getallUser();
   }, []);
+  const [productData, setProductData] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [expandedProductId, setExpandedProductId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1); // For pagination
+  const toggleProductName = (itemId) => {
+    setExpandedProductId(expandedProductId === itemId ? null : itemId);
+  };
 
+  const fetchOrders = async () => {
+    setLoad(true);
+    try {
+      const response = await axios.get(
+        "https://villyzstore.onrender.com/allOrders"
+      );
+      console.log(response);
+
+      if (response) {
+        setProductData(response.data);
+      } else {
+        alert("Error fetching orders");
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoad(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+  const itemsPerPage = 5;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentProducts = productData.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const totalPages = Math.ceil(productData.length / itemsPerPage);
+  const paginationButtons = Array.from({ length: totalPages }, (_, index) => (
+    <button
+      key={index}
+      onClick={() => paginate(index + 1)}
+      className={`btn ${
+        currentPage === index + 1 ? "padination-active" : "padination-inactive"
+      }`}
+      style={{ margin: "5px" }}
+    >
+      {index + 1}
+    </button>
+  ));
+  const handleDelete = async (itemId) => {
+    setLoad(true);
+    try {
+      const response = await axios.delete(
+        `https://villyzstore.onrender.com/deleteOrder/${itemId}`
+      );
+      if (response) {
+        alert.alert("order deleted successfully");
+        fetchOrders();
+      }
+    } catch (error) {
+      console.error("Delete error:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Failed to delete the item.",
+      });
+    } finally {
+      setLoad(false);
+    }
+  };
+  const [orderStatus, setOrderStatus] = useState("");
+  useEffect(() => {
+    if (selectedOrder) {
+      setOrderStatus(selectedOrder.orderStatus || "Processing");
+    }
+  }, [selectedOrder]);
+
+  const handleStatusChange = async (event) => {
+    const newStatus = event.target.value;
+    console.log("Selected Status:", newStatus); // Debugging log
+
+    setOrderStatus(newStatus);
+
+    if (!selectedOrder) return;
+
+    try {
+      const response = await fetch(
+        `https://villyzstore.onrender.com/updateOrderStatus/${selectedOrder._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ orderStatus: newStatus }),
+        }
+      );
+
+      const data = await response.json();
+      console.log("Backend Response:", data); // Debugging log
+
+      if (!response.ok) throw new Error("Failed to update order status");
+
+      setSelectedOrder((prev) => ({ ...prev, orderStatus: newStatus }));
+      alert("Order status updated successfully!");
+      fetchOrders();
+    } catch (error) {
+      console.error(error);
+      alert("Failed to update status");
+    }
+  };
   return (
     <div>
       <div className="congratulation-Container">
@@ -450,28 +568,312 @@ const Landing = () => {
             </div>
           </div>
           <div className="mt-2">
-            {prod.slice(0, 6).map((item) => {
-              return (
-                <div className="d-flex align-items-center justify-content-between mt-3">
-                  <div className="d-flex align-items-center gap-4">
-                    <div>
-                      <img src={item.image} alt="" width={40} height={50} />
-                    </div>
-                    <div>
-                      <div className="Product-Name">
-                        {item.productName.slice(0, 20)}...
+            {Prodloader ? (
+              <div className="loading-text">loading....</div>
+            ) : (
+              prod.slice(0, 6).map((item) => {
+                return (
+                  <div className="d-flex align-items-center justify-content-between mt-3">
+                    <div className="d-flex align-items-center gap-4">
+                      <div>
+                        <img src={item.image} alt="" width={40} height={50} />
                       </div>
-                      <div className="product-ID">
-                        item : FXY {item._id.slice(0, 4)}
+                      <div>
+                        <div className="Product-Name">
+                          {item.productName.slice(0, 20)}...
+                        </div>
+                        <div className="product-ID">
+                          item : FXY {item._id.slice(0, 4)}
+                        </div>
                       </div>
                     </div>
+                    <div className="Product-price">${item.newPrice}</div>
                   </div>
-                  <div className="Product-price">${item.newPrice}</div>
-                </div>
-              );
-            })}
+                );
+              })
+            )}
           </div>
         </div>
+      </div>
+      <div className="Orders-container">
+        <div className="order-head">
+          <div className="d-flex align-items-center gap-3">
+            <div className="d-flex gap-2 align-items-center">
+              <div className="Show-btn">Show</div>
+              <div>
+                <select name="" className="form-select" id="">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                </select>
+              </div>
+            </div>
+            <button className="order-button">
+              <IoIosAdd size={22} />
+              Show all Orders
+            </button>
+          </div>
+          <div className="d-flex gap-3 align-items-center">
+            <select name="" className="form-select p-2" id="">
+              <option value="">Status</option>
+              <option value="">Shipped</option>
+              <option value="">Delivered</option>
+              <option value="">Canceled</option>
+              <option value="">Processing</option>
+            </select>
+            <div>
+              <input
+                type="text"
+                className="form-control order-form"
+                name=""
+                placeholder="search by orderID"
+                id=""
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="table-container ">
+            <table>
+              <thead>
+                <tr className="table-head">
+                  <th>
+                    <input type="checkbox" />
+                  </th>
+                  <th>#</th>
+                  <th>Status</th>
+                  <th>Total</th>
+                  <th className="table-Date">Issue Date</th>
+                  <th>Delivery</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {load ? (
+                  <p className="loading-text">loading....</p>
+                ) : (
+                  currentProducts.map((product) => (
+                    <tr>
+                      <td>
+                        <input type="checkbox" />
+                      </td>
+                      <td className="OrderCode">
+                        #FX{product._id.slice(0, 5)}
+                      </td>
+                      <td>
+                        <div
+                          className={
+                            product.orderStatus === "Processing"
+                              ? "table-Status-pending"
+                              : product.orderStatus === "Cancelled"
+                              ? "table-Status-Cancelled"
+                              : "table-Status"
+                          }
+                        >
+                          {product.orderStatus}
+                        </div>
+                      </td>
+                      <td className="table-text">${product.OrderPrice}</td>
+                      <td className="table-Date">
+                        {product.date.slice(0, 10)}
+                      </td>
+                      <td className="table-text">${product.DeliveryFee}</td>
+                      <td>
+                        <div className="d-flex align-items-center ">
+                          <div
+                            className="eye-container"
+                            type="button"
+                            data-bs-toggle="modal"
+                            data-bs-target="#staticBackdrop"
+                            onClick={() => setSelectedOrder(product)}
+                          >
+                            <LuEye className="icon-Eye" />
+                          </div>
+                          <div
+                            className="icon-Trash-container"
+                            onClick={() => handleDelete(product._id)}
+                          >
+                            <HiOutlineTrash className="icon-Trash" />
+                          </div>
+                          <div className="icon-DOts-container">
+                            <PiDotsThreeVerticalBold className="icon-DOts" />
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+            <div
+              class="modal fade"
+              id="staticBackdrop"
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabindex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog">
+                {selectedOrder ? (
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                        Order details
+                      </h1>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Customer name</div>
+                          <div>{selectedOrder.name}</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Customer ID</div>
+                          <div>{selectedOrder.UserID}</div>
+                        </div>
+
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Order ID</div>
+                          <div>{selectedOrder._id}</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Order date</div>
+                          <div>{selectedOrder.date}</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Payment Reference</div>
+                          <div>{selectedOrder.paymentReference}</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Order price</div>
+                          <div>${selectedOrder.OrderPrice}</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Delivery Fee</div>
+                          <div>${selectedOrder.DeliveryFee}</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Payment method</div>
+                          <div>Card</div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Payment status</div>
+                          <div
+                            className={
+                              selectedOrder.PaymentStatus === "Paid"
+                                ? "success"
+                                : "pending"
+                            }
+                          >
+                            {selectedOrder.orderStatus}
+                          </div>
+                        </div>
+                        <select
+                          id="status"
+                          value={orderStatus}
+                          onChange={handleStatusChange}
+                        >
+                          <option value="Processing">Processing</option>
+                          <option value="Shipping">Shipping</option>
+                          <option value="Shipped">Shipped</option>
+                          <option value="Delivered">Delivered</option>
+                          <option value="Cancelled">Cancelled</option>
+                        </select>
+                        <div className="d-flex align-items-center justify-content-between mt-3">
+                          <div>Order status</div>
+                          <div className="text-danger">
+                            {selectedOrder.orderStatus}
+                          </div>
+                        </div>
+                      </>
+                    </div>
+
+                    <hr />
+                    <div className="px-3 pb-3">
+                      <div className="modal-head">order details</div>
+                      <div className="order-details my-3">
+                        {selectedOrder.Orders.map((item) => {
+                          return (
+                            <div>
+                              <div className="d-flex align-items-center">
+                                <div>
+                                  <img
+                                    src={item.image}
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                  />
+                                </div>
+                                <div>
+                                  {expandedProductId === item._id
+                                    ? item.name
+                                    : item.name.length > 15
+                                    ? `${item.name.slice(0, 15)}...`
+                                    : item.name}
+                                  {item.name.length > 15 && (
+                                    <span
+                                      onClick={() =>
+                                        toggleProductName(item._id)
+                                      }
+                                      style={{
+                                        color: "blue",
+                                        cursor: "pointer",
+                                        marginLeft: "5px",
+                                      }}
+                                    >
+                                      {expandedProductId === item._id
+                                        ? "Read less"
+                                        : "Read more"}
+                                    </span>
+                                  )}
+                                </div>
+                              </div>
+                              <hr />
+                              <div className="mt-2 ms-2">
+                                Quantity : {item.quantity}
+                              </div>
+                              <hr />
+                              <div className="mt-2 ms-2 mb-1">
+                                Price:${item.price}
+                              </div>
+                              <hr />
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="modal-head px-3 pb-1 ">
+                      shipping address
+                    </div>
+                    <div className="address px-4 pb-5">
+                      <div>Country : {selectedOrder.country}</div>
+                      <div>Address :{selectedOrder.street}</div>
+                      <div>State : {selectedOrder.state}</div>
+                      <div>Postal Code : {selectedOrder.postalCode}</div>
+                      <div>city : {selectedOrder.city}</div>
+                    </div>
+                  </div>
+                ) : (
+                  <p className="loading-text">Loading....</p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        {load ? null : (
+          <div style={{ textAlign: "end" }}>{paginationButtons}</div>
+        )}
       </div>
     </div>
   );
